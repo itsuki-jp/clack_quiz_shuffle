c2318cc52725f59e2878368b4dac16c3d8631770
itsuki-jp <itsuki19601@gmail.com>
2022-02-11 22:28:18 +0900

    コメントの追加

--- rank/main.js
+++ rank/main.js
@@ -43,33 +43,35 @@ const quizSentence = document.getElementById('quiz_sentence');
 const quizAnswerSelect = document.getElementById('quiz_answer_select');
 const quizSendButton = document.getElementById('quiz_send_button');
 const quizResult = document.getElementById('quiz_result');
+// 正解数を保存する変数
 let correct_num = 0;
 
-
+// 再帰出来るように関数として定義した
+// idx => 何番目の問題を見るか
 function func(idx) {
+    // indが用意した問題の数以上に成れば、正解数等を表示する
     if (idx === quizData.length) {
+        // 要らない要素を削除する
         quizSentence.remove();
         quizAnswerSelect.remove();
         quizSendButton.remove();
+        // 正解数とランクを表示する（ランクの判定はここで行えば良い）
         quizResult.innerText = `正解数 : ${correct_num}\nRank : SSS`;
+        // quizResultにクラス名 end を付与
         quizResult.classList.add("end");
+        // 再帰を終了する
         return;
     }
     const useQuizData = quizData[idx];
     quizResult.innerHTML = "";
     quizAnswerSelect.innerHTML = "";
-    // 選択肢
     for (const choice of useQuizData.choices) {
         quizAnswerSelect.innerHTML += `<input id="${choice.id}" class="radio-inline__input" type="radio" name="answer" value="${choice.id}"><label class="radio-inline__label" for="${choice.id}">${choice.value}</label>`
     }
 
-    // 問題文
     quizSentence.innerHTML = `<p>${useQuizData.title}</p>`
-        // ボタン押下時
     quizSendButton.onclick = ev => {
-        // 選択肢を全部取得
         for (const element of quizAnswerSelect.querySelectorAll("input")) {
-            // 選択肢の状態を確認し、チェック状態なら判定
             if (element.checked) {
                 const result = element.id == useQuizData.correctId;
                 if (result) {
@@ -79,10 +81,11 @@ function func(idx) {
                 quizResult.innerHTML = `<p>${str}</p>`
             }
         }
+        // 回答ボタンを押した一秒後に次の問題に移る
         setTimeout(() => {
             func(idx + 1)
         }, 1000);
 
     };
 }
-func(3);
+func(0);
